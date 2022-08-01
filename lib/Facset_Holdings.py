22e8abacb7e071d740849d88b3cc799e3f08dddb
Andres Hsiao 蕭瑾玟 <Andres.Hsiao@nomurafunds.com.tw>
2022-08-01 14:02:12 +0800

    functionalize factset_holding and jepun_data

--- lib/Facset_Holdings.py
+++ lib/Facset_Holdings.py
@@ -1,4 +1,4 @@
-from datatable import f, dt
+from datatable import f, dt, last
 import os
 import pyodbc
 from datetime import datetime
@@ -41,6 +41,86 @@ def dt_time_format_transform(frame:dt.Frame, from_type) -> dt.Frame:
 
     return frame
 
+def save_facset_holdings_data(facset_holdings_frame, cursor):
+    last_date = 0
+
+    for i in range(facset_holdings_frame.nrows):
+        row = facset_holdings_frame[i, :]
+        
+        # assign column values to variables
+        port_id = row[0,0]
+        date_ = row[0,1]
+        ISIN = row[0,2]
+        SEDOL = row[0,3]
+        CUSIP = row[0,4]
+        asset_name = row[0,5].replace("'", "''")
+        asset_class = row[0,6]
+
+        # log for tracking the process
+        if date_ != last_date or last_date == 0:
+            print(f'Facset holdings: {date_} processing...') 
+
+        # SQL queries for inserting data into SQL Server DB       
+        queries = (
+        f"IF NOT EXISTS (SELECT * FROM factset_holdings \n"
+        f"WHERE port_id='{port_id}' AND date_='{date_}' AND asset_name='{asset_name}') \n"
+        f"BEGIN \n"
+        f"INSERT INTO factset_holdings (port_id, date_, ISIN, SEDOL, \n"
+        f"CUSIP, asset_name, asset_class) \n"
+        f"VALUES ('{port_id}', '{date_}', '{ISIN}', '{SEDOL}',\n"
+        f"'{CUSIP}', '{asset_name}', '{asset_class}') \n"
+        F"END"
+        )
+
+        try:
+            cursor.execute(queries)
+            last_date = date_
+        
+        except Exception as err:
+            error_message = f'Factset Holdings: port_id={port_id}, date={date_}, ISIN={ISIN}, name={asset_name}\n {err}\n'
+            error_log += error_message
+
+    cursor.commit()
+
+def save_jepun_data(jepun_frame, cursor):
+    last_date = 0
+
+    for i in range(jepun_frame.nrows):
+        row = jepun_frame[i, :]
+
+        # assign column values to variables
+        port_id = row[0,0]
+        date_ = row[0,1]
+        ISIN = row[0,2]
+        wgt = row[0,3]
+        asset_name = row[0,4].replace("'", "''") 
+
+        # log for tracking the process
+        if date_ != last_date or last_date == 0:
+            print(f'Jepun: {date_} processing...') 
+
+        # query for insert jepun data into DB
+        queries = (
+        f"BEGIN TRY \n"
+        f"INSERT INTO jepun_data \n"
+        f"VALUES ('{port_id}', '{date_}', '{ISIN}', \n" 
+        f"'{wgt}', '{asset_name}') \n"
+        f"END TRY \n"
+        f"BEGIN CATCH \n"
+        f"END CATCH"
+        )
+
+        try:
+            cursor.execute(queries)
+            last_date = date_
+        
+        except Exception as err:
+            error_message = f'Jepun: port_id={port_id}, date={date_}, ISIN={ISIN}, name={asset_name}\n {err}\n'
+            error_log += error_message
+
+    cursor.commit() 
+
+
 if __name__ == '__main__':
     RMDB_path = r'\\sptwap00004\DTIReport\\ERMFiles\\RMDB\\Upload'
 
@@ -52,67 +132,77 @@ if __name__ == '__main__':
     cursor = cnxn.cursor()
 
     latest_date = cursor.execute('select max(date_) as md from factset_holdings').fetchval()
+    print(f'Begin date: {latest_date}')
 
     error_log = ''
 
     factset, jepun = get_holdings_data(RMDB_path, latest_date)
 
+    save_facset_holdings_data(factset, cursor)
+    save_jepun_data(jepun, cursor)
+
+    # date_, last_date = 0, 0
     
-    for i in range(factset.nrows):
-        row = factset[i, :]
-        port_id = row[0,0]
-        date_ = row[0,1]
-        ISIN = row[0,2]
-        SEDOL = row[0,3]
-        CUSIP = row[0,4]
-        asset_name = row[0,5].replace("'", "''")
-        asset_class = row[0,6]
+    # for i in range(factset.nrows):
+    #     row = factset[i, :]
+    #     port_id = row[0,0]
+    #     date_ = row[0,1]
+    #     ISIN = row[0,2]
+    #     SEDOL = row[0,3]
+    #     CUSIP = row[0,4]
+    #     asset_name = row[0,5].replace("'", "''")
+    #     asset_class = row[0,6]
 
-        queries = (
-        # f"BEGIN TRY \n"
-        f"IF NOT EXISTS (SELECT * FROM factset_holdings \n"
-        f"WHERE port_id='{port_id}' AND date_='{date_}' AND asset_name='{asset_name}') \n"
-        f"BEGIN \n"
-        f"INSERT INTO factset_holdings (port_id, date_, ISIN, SEDOL, \n"
-        f"CUSIP, asset_name, asset_class) \n"
-        f"VALUES ('{port_id}', '{date_}', '{ISIN}', '{SEDOL}',\n"
-        f"'{CUSIP}', '{asset_name}', '{asset_class}') \n"
-        F"END"
-        # f"END TRY \n"
-        # f"BEGIN CATCH \n"
-        # f"END CATCH"
-        )
+    #     if date_ != last_date or last_date == 0:
+    #         print(f'Facset holdings: {date_} processing...')
 
-        print(queries)
+    #     queries = (
+    #     # f"BEGIN TRY \n"
+    #     f"IF NOT EXISTS (SELECT * FROM factset_holdings \n"
+    #     f"WHERE port_id='{port_id}' AND date_='{date_}' AND asset_name='{asset_name}') \n"
+    #     f"BEGIN \n"
+    #     f"INSERT INTO factset_holdings (port_id, date_, ISIN, SEDOL, \n"
+    #     f"CUSIP, asset_name, asset_class) \n"
+    #     f"VALUES ('{port_id}', '{date_}', '{ISIN}', '{SEDOL}',\n"
+    #     f"'{CUSIP}', '{asset_name}', '{asset_class}') \n"
+    #     F"END"
+    #     # f"END TRY \n"
+    #     # f"BEGIN CATCH \n"
+    #     # f"END CATCH"
+    #     )
 
-        try:
-            cursor.execute(queries)
+    #     # print(queries)
+
+    #     try:
+    #         cursor.execute(queries)
+    #         last_date = date_
         
-        except Exception as err:
-            error_message = f'port_id={port_id}, date={date_}, ISIN={ISIN}, name={asset_name}\n {err}\n'
-            error_log += error_message
-            continue
+    #     except Exception as err:
+    #         error_message = f'port_id={port_id}, date={date_}, ISIN={ISIN}, name={asset_name}\n {err}\n'
+    #         error_log += error_message
+    #         continue
 
-    for i in range(jepun.nrows):
-        row = jepun[i, :]
-        row[0,4] = row[0, 4].replace("'", "''")
-        queries = (
-        f"BEGIN TRY \n"
-        f"INSERT INTO jepun_data \n"
-        f"VALUES ('{row[0,0]}', '{row[0,1]}', '{row[0,2]}', '{row[0,3]}',\n"
-        f"'{row[0,4]}') \n"
-        f"END TRY \n"
-        f"BEGIN CATCH \n"
-        f"END CATCH"
-        )
-        cursor.execute(queries)
+    # for i in range(jepun.nrows):
+    #     row = jepun[i, :]
+    #     row[0,4] = row[0, 4].replace("'", "''")
+    #     queries = (
+    #     f"BEGIN TRY \n"
+    #     f"INSERT INTO jepun_data \n"
+    #     f"VALUES ('{row[0,0]}', '{row[0,1]}', '{row[0,2]}', '{row[0,3]}',\n"
+    #     f"'{row[0,4]}') \n"
+    #     f"END TRY \n"
+    #     f"BEGIN CATCH \n"
+    #     f"END CATCH"
+    #     )
+    #     cursor.execute(queries)
+        
 
-    cursor.commit()     
+    # cursor.commit()     
 
     with open('error_log.txt', 'wt') as log:
         log.write(error_log)
     
     log.close()
     
-    print(factset)
-    print(jepun)
+    # print(factset)
+    # print(jepun)
